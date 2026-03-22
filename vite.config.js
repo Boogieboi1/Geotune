9d3bfa2b251d8de71dbecebfc4872e88d53f476d
Boogieboi <baz_graham@live.nl>
2026-03-22 17:00:15 +0100

    dgedg

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,43 @@
+import react from '@vitejs/plugin-react'
+import { transformWithEsbuild } from 'vite'
 import restart from 'vite-plugin-restart'
-import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
-  root: 'src/',
-  publicDir: '../public/',
-  server: {
-    host: true,
-    open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
-  },
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    sourcemap: true
-  },
-  plugins: [
-    restart({ restart: ['../public/**'] })
-  ]
-})
+export default {
+    base: process.env.NODE_ENV === 'production' ? '/' : '/',
+    root: 'src/',
+    publicDir: '../public/',
+    plugins:
+    [
+        // Restart server on static/public file change
+        restart({ restart: [ '../public/**', ] }),
+
+        // React support
+        react(),
+
+        // .js file support as if it was JSX
+        {
+            name: 'load+transform-js-files-as-jsx',
+            async transform(code, id)
+            {
+                if (!id.match(/src\/.*\.js$/))
+                    return null
+
+                return transformWithEsbuild(code, id, {
+                    loader: 'jsx',
+                    jsx: 'automatic',
+                });
+            },
+        },
+    ],
+    server:
+    {
+        host: true, // Open to local network and display URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: '../dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true // Add sourcemap
+    },
+}
